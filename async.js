d8a77b48521de172d86e35ecdd111e7381811e5d
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2023-12-27 14:48:56 +0200

    complet the Example

--- async.js
+++ async.js
@@ -1,13 +1,20 @@
 const https = require('https');
 
 const start = Date.now();
-// send request to google
-https.request('https://www.google.com', res => {
-    res.on('date', ()=> {
 
-    });
+const doRequest = () => {
+    https.request('https://www.google.com', res => {
+        res.on('data', () => {})
+    
+        res.on('end', () => {
+            console.log('time', Date.now() - start);
+        })
+    }).end()
+}
 
-    res.on('end', () => {
-        console.log('Time',Date.now() - start);
-    });
-}).end();
+doRequest()
+doRequest()
+doRequest()
+doRequest()
+doRequest()
+doRequest()
