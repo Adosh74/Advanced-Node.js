15578449de9391cd30766ba930642c9c9091cc2c
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-16 01:18:03 +0200

    + remove cluster

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const cluster = require('cluster');
 const os = require('os');
+const crypto = require('crypto');
 const express = require('express');
 
 
+const app = express();
 
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
-    const cpuCount = os.cpus().length;
-    console.log(`Forking for ${cpuCount} CPUs`);
-    for (let i = 0; i < cpuCount; i++) {
-        cluster.fork();
-    }    
-} else {
-    
-    const app = express();
-    const doWork = (duration) => {
-        const start = Date.now();
-        while (Date.now() - start < duration) {}
-    }
-
-    app.get('/', (req, res) => {
-        doWork(5000);
+app.get('/', (req, res) => {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
         res.status(200).send('Hi')
-    } )
-
-    app.get('/fast', (req, res) => {
-        res.status(200).send('Fast')
     })
+} )
 
-    app.listen(4000, () => {
-        console.log('app running on port 4000');
-    })
-}
+app.get('/fast', (req, res) => {
+    res.status(200).send('Fast')
+})
+
+app.listen(4000, () => {
+    console.log('app running on port 4000');
+})
+
