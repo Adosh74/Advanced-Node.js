ac4ba8dad3da3d11fd511205e3178dc221637d24
Mohamed Hamdy <98131964+Adosh74@users.noreply.github.com>
2024-01-15 06:13:33 +0200

    + add example of heavy

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+const doWork = (duration) => {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+}
+
 app.get('/', (req, res) => {
+    doWork(5000);
     res.status(200).send('Hi')
 } )
 
